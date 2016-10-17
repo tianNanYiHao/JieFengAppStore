92d114e1ec02897f2e0809ae0986493e269122d5
tianNanYiHao <851085835@qq.com>
2016-10-17 17:30:40 +0800

    123

---
+++ 2.1.4/QuickPos/QuickPos/ViewControllers/TrainTicket/view/TrickPersonListView.h
@@ -0,0 +1,17 @@
+//
+//  TrickPersonListView.h
+//  QuickPos
+//
+//  Created by Lff on 16/10/17.
+//  Copyright © 2016年 张倡榕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrickPersonListView : UIView
+
+
+-(instancetype)initWithFrame:(CGRect)frame;
+
+
+@end

43491661eb95d262ade584b83f1c52f11a63bc84
tianNanYiHao <851085835@qq.com>
2016-10-20 16:23:42 +0800

    123

---
+++ 2.1.4/QuickPos/QuickPos/ViewControllers/TrainTicket/model/TrickInfoModel.h
@@ -0,0 +1,17 @@
+//
+//  TrickInfoModel.h
+//  QuickPos
+//
+//  Created by Lff on 16/10/20.
+//  Copyright © 2016年 张倡榕. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TrickInfoModel : NSObject
+@property (nonatomic,strong) NSString *train_no ;
+
+
+
+
+@end

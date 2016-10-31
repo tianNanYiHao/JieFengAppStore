6002487d5aa43b7b49caa6c25141945541e3d0d7
tianNanYiHao <851085835@qq.com>
2016-10-31 15:00:16 +0800

    123

--- 2.1.4/QuickPos/QuickPos/ViewControllers/ZFBPay/ZFBViewController.m
+++ 2.1.4/QuickPos/QuickPos/ViewControllers/ZFBPay/ZFBViewController.m
@@ -12,7 +12,14 @@
 #import "XYSwitch.h"
 #import "UIImageView+CornerRadius.h"
 #import "Request.h"
-
+#import "UMSocial.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WechatAuthSDK.h"
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
+#import <ShareSDKUI/SSUIShareActionSheetCustomItem.h>
+#import <ShareSDK/ShareSDK+Base.h>
 
 @interface ZFBViewController ()<ResponseData>
 {
@@ -24,7 +31,7 @@
     Request *req;
     NSString *payTool;
     NSString *merchorder_No;
-    
+    UILongPressGestureRecognizer *singleTap;
     
 }
 @property (weak, nonatomic) IBOutlet UILabel *openShowLab1;//
@@ -106,6 +113,7 @@
     if (type == REQUSET_ORDER) {
         if ([[dict objectForKey:@"respCode"] isEqualToString:@"0000"]) {
             _ewmImageViw.hidden = NO;
+            [self ImageViewAndTap];
         }else{
             [Common showMsgBox:nil msg:[dict objectForKey:@"respDesc"] parentCtrl:self];
         }
@@ -184,5 +192,72 @@
     //        [MBProgressHUD showHUDAddedTo:self.view WithString:@"二维码获取中..."];
     //    }
 }
+//设置imageView长按手势分享二维码
+- (void)ImageViewAndTap{
+    _ewmImageViw.userInteractionEnabled = YES;
+    singleTap=[[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(shareClickS:)];
+    [self.ewmImageViw addGestureRecognizer:singleTap];
+    //    [singleTap release];
+    //长按手势
+    singleTap.minimumPressDuration=1;
+    //所需触摸1次
+    singleTap.numberOfTouchesRequired=1;
+}
 
+//调整长按手势触发两次的问题
+-(void)shareClickS:(UILongPressGestureRecognizer *)sender
+
+{
+    
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        //1、创建分享参数
+        NSArray* imageArray = @[self.ewmImageViw.image];
+        
+        if (imageArray) {
+            NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+            [shareParams SSDKSetupShareParamsByText:@"分享内容"
+                                             images:imageArray
+                                                url:[NSURL URLWithString:@"www.jiefengpay.com"]
+                                              title:@"分享标题"
+                                               type:SSDKContentTypeImage];
+            //2,分享
+            [ShareSDK showShareActionSheet:sender
+                                     items:nil
+                               shareParams:shareParams
+                       onShareStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
+                           
+                           switch (state)
+                           {
+                               case SSDKResponseStateSuccess:
+                               {
+                                   UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                                       message:nil
+                                                                                      delegate:nil
+                                                                             cancelButtonTitle:@"确定"
+                                                                             otherButtonTitles:nil];
+                                   [alertView show];
+                                   break;
+                               }
+                               case SSDKResponseStateFail:
+                               {
+                                   UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                                       message:[NSString stringWithFormat:@"%@", error]
+                                                                                      delegate:nil
+                                                                             cancelButtonTitle:@"确定"
+                                                                             otherButtonTitles:nil];
+                                   [alertView show];
+                                   break;
+                               }
+                            break;
+                           }
+                       }];
+        }
+    }
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    _ewmImageViw.userInteractionEnabled = NO;
+    [_ewmImageViw removeGestureRecognizer:singleTap];
+}
 @end
